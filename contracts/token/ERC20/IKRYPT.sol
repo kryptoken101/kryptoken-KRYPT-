3a4c50491f80cb72a78d38a64bb8156bf111cc36
KRYPTOKEN <kriscripting17@gmail.com>
2022-06-17 05:07:24 +0800

    Update IKRYPT.sol

--- contracts/token/ERC20/IKRYPT.sol
+++ contracts/token/ERC20/IKRYPT.sol
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
 contract IKRYPT is ERC20 {
     event Deposit(address indexed sender, uint256 amount);
     event Withdrawal(address indexed recipient, uint256 amount);
