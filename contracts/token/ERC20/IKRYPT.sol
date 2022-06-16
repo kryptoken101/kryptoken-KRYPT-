542ad47c1191a90725271926a60dd6c0658bc415
KRYPTOKEN <kriscripting17@gmail.com>
2022-06-16 23:09:23 +0800

    Create IKRYPT.sol

---
+++ contracts/token/ERC20/IKRYPT.sol
@@ -0,0 +1,10 @@
+contract IKRYPT is ERC20 {
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdrawal(address indexed recipient, uint256 amount);
+
+    function deposit() public payable;
+
+    function withdraw(uint256 amount) public;
+
+    function withdraw(uint256 amount, address user) public;
+}
